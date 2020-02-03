c9b5bcc019abbbcce18264254edde3346cdffaba
Pandu Cahyo Pangestu <pandusudo@gmail.com>
2020-02-03 18:18:06 +0700

    Add 8Kyu

---
+++ Python/8Kyu/BasicMathOp.py
@@ -0,0 +1,13 @@
+def basic_op(operator, value1, value2):
+    if operator == '+':
+        result = value1 + value2
+        return result
+    elif operator == '-':
+        result = value1 - value2
+        return result
+    elif operator == '*':
+        result = value1 * value2
+        return result
+    elif operator == '/':
+        result = value1 / value2
+        return result
